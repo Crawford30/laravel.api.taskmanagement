bb75a3ef0671932d1047af0be153879d8da1e631
Joel Crawford <joelcrawford30@gmail.com>
2024-06-21 11:53:17 +0300

    Add Task Priotoies as config to allow fo =r future addition

---
+++ config/taskpriorities.php
@@ -0,0 +1,15 @@
+<?php
+  /*
+    |--------------------------------------------------------------------------
+    | TaskPriorities
+    |--------------------------------------------------------------------------
+    |
+    | This part define the priorities of tasks which can be added and removed and doesnt need one to
+    | to add a migration file to add the priorities  to tasks.
+    | This provides for code reusbility
+    |
+    */
+
+return [
+    'task_priorities'   => ['low', 'medium', 'high'],
+];
