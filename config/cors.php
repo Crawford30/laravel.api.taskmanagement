2f95c123bae29686050f9410dc19605dc45b4c10
Joel Crawford <joelcrawford30@gmail.com>
2024-06-24 18:12:01 +0300

    Cors.php

--- config/cors.php
+++ config/cors.php
@@ -31,4 +31,20 @@ return [
 
     'supports_credentials' => false,
 
+    // 'paths' => ['api/*', 'passport/csrf-cookie'],
+
+    // 'allowed_methods' => ['*'],
+
+    // 'allowed_origins' => ['*'],
+
+    // 'allowed_origins_patterns' => [],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => false,
+
 ];
