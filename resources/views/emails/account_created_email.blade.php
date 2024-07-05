1c8735f225975d94a7f30bcfd3bd3cfc657496cf
Joel Crawford <joelcrawford30@gmail.com>
2024-07-05 11:52:05 +0300

    Email Structure

--- resources/views/emails/account_created_email.blade.php
+++ resources/views/emails/account_created_email.blade.php
@@ -2,13 +2,13 @@
 Hello {{ $name }},
 
 A user account has been
-<b>created</b> for you on {{ env('APP_NAME') }} with the following credentials.
+<b>created</b> for you on <strong>{{ env('APP_NAME') }}</strong> with the following credentials.
 
 <strong>Email:</strong> {{ $email }}<br>
 <strong>Password:</strong> {{ $password }}
 
 Regards,<br>
-PC Team
+{{ env('APP_NAME') }} Team
 </x-mail::message>
 
 
